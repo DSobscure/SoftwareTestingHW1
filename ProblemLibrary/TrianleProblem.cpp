ee8e414fb215d84b2661756418b506cf6ad014b2
DSobscure <snbruce13@yahoo.com.tw>
2017-03-20 17:50:00 +0800

    TriangleProblemSolved

--- ProblemLibrary/TrianleProblem.cpp
+++ ProblemLibrary/TrianleProblem.cpp
@@ -11,5 +11,14 @@ TriangleProblem::~TriangleProblem()
 
 std::string TriangleProblem::TriangleType(int a, int b, int c)
 {
-	return "Not Implement";
+	if (a <= 0 || b <= 0 || c <= 0)
+		return "not a triangle";
+	else if (a >= b + c || b >= a + c || c >= a + b)
+		return "not a triangle";
+	else if (a == b && b == c)
+		return "equilateral";
+	else if (a == b || b == c || a == c)
+		return "isosceles";
+	else
+		return "scalene";
 }
